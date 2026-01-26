ec58a88ac681f71c2745927d43a34909108359ee
yis-cmd <0361630@gmail.com>
2026-01-26 20:37:09 +0200

    level 2

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
-from space_network_lib import Packet, SpaceEntity, SpaceNetwork
+from time import sleep
+from space_network_lib import DataCorruptedError, Packet, SpaceEntity, SpaceNetwork, TemporalInterferenceError
 
 
 class Sattelite(SpaceEntity):
     def receive_signal(self, packet: Packet):
         print(f"[{self.name}] Received: {packet}")
 
+def attempt_transmission(message:Packet):
+    while True:
+        try:
+            network.send(message)
+            return
+        except TemporalInterferenceError:
+            print("Interference, waiting...")
+            sleep(2)
+        except DataCorruptedError:
+            print("Data corrupted, retrying...")
 
+    
 
-
-def main():
-    network = SpaceNetwork(level=1)
+if __name__ == "__main__":
+    network = SpaceNetwork(level=2)
     sat1 = Sattelite("sat1", 100)
     sat2 = Sattelite("sat2", 200)
     message = Packet("cwfvsdgkbuydxyvfbuxrhj", sat1, sat2)
-    network.send(message)
-
-if __name__ == "__main__":
-    main()
+    attempt_transmission(message)
