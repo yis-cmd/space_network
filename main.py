0bb9ecb0f67c71a23f775bef767272d58a7a81f9
yis-cmd <0361630@gmail.com>
2026-01-27 13:33:03 +0200

    level-6

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+from __future__ import annotations
 from time import sleep
+
 from space_network_lib import DataCorruptedError, LinkTerminatedError, OutOfRangeError, Packet, SpaceEntity, SpaceNetwork, TemporalInterferenceError
 
 class BrokenConnectionError(Exception):
     pass
 
 class RelayPacket(Packet):
-    def __init__(self, packet_to_relay, sender, proxy):
+    def __init__(self, packet_to_relay:Packet | RelayPacket, sender:SpaceEntity, proxy:SpaceEntity):
         super().__init__(packet_to_relay, sender, proxy)
     
     def __repr__(self):
+        # return f"{self.sender} {self.receiver}"
         return f"RelayPacket(Relaying [{self.data}] to {self.receiver} from {self.sender})"
 
 class Satellite(SpaceEntity):
@@ -40,19 +43,35 @@ def attempt_transmission(message:Packet):
         except OutOfRangeError:
             print("Target out of range")
             raise BrokenConnectionError
+    
+def find_route(space_entities:list[SpaceEntity], message:Packet)-> Packet | RelayPacket:
+    assert isinstance(message.sender, SpaceEntity)
+    assert isinstance(message.receiver, SpaceEntity)
+    if message.sender.distance_from_earth + 150 >= message.receiver.distance_from_earth:
+        return message
+    for entity in space_entities:
+        if entity.distance_from_earth > message.sender.distance_from_earth and entity.distance_from_earth - message.sender.distance_from_earth <= 150:
+            proxy = entity
+            break
+    if proxy is None:
+        raise OutOfRangeError
+    new_message = find_route(space_entities, Packet(message.data, proxy, message.receiver))
+    return RelayPacket(new_message, message.sender, proxy)
+
+def smart_send_packet(space_entities:list[SpaceEntity], message:Packet):
+    to_send = find_route(space_entities, message)
+    attempt_transmission(to_send)
 
 if __name__ == "__main__":
-    network = SpaceNetwork(level=5)
+    network = SpaceNetwork(level=6)
     sat1 = Satellite("sat1", 100)
     sat2 = Satellite("sat2", 200)
     sat3 = Satellite("sat3", 300)
     sat4 = Satellite("sat4", 400)
     earth = Earth("earth", 0)
-    p_final = Packet("Hello from Earth!!", sat3, sat4)
-    p_sat2_to_sat3 = RelayPacket(p_final, sat2, sat3)
-    p_sat1_to_sat2 = RelayPacket(p_sat2_to_sat3, sat1, sat2)
-    p_earth_to_sat1 = RelayPacket(p_sat1_to_sat2, earth, sat1)
+    space_entities = [sat1, sat2, sat3, sat4, earth]
+    p_final = Packet("Hello from Earth!!", earth, sat4)
     try:
-        attempt_transmission(p_earth_to_sat1)
+        smart_send_packet(space_entities, p_final)
     except BrokenConnectionError:
         print("Transmission failed")
